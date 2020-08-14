33f5b49b10bf72631fc0a4096ca5f4b606b4379e
whddn5464 <rlawhddn5464@naver.com>
2020-08-14 21:10:36 +0900

    java

---
+++ js/example.js
@@ -0,0 +1,2 @@
+document.write("인생\n");
+document.write("인생123123");
---
+++ js/test.js
@@ -0,0 +1,5 @@
+document.write("환영합니다.","<br>");
+var menu = ["짜장면","된장국","김치찌개","회덮밥"];
+var menuNum = Math.floor(Math.random()*4);
+var result = menu[menuNum];
+document.write(result);
